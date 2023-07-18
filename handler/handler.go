bd99fdbe45167184cdd67aae923548e41dd0e7e1
Verrol Adams <verrol@lorrev.org>
2023-07-18 09:23:29 -0400

    implementation of handler.GetAllProducts

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,53 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/verrol/go-rest-api-with-fiber/database"
+	"github.com/verrol/go-rest-api-with-fiber/model"
+)
 
 func GetAllProducts(c *fiber.Ctx) error {
-	return c.SendString("got all products")
+	// query product table in the db
+	query := `SELECT * FROM products order by name`
+	rows, err := database.DB.Query(query)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"status": false,
+			"err":    err,
+		})
+	}
+
+	defer rows.Close()
+	result := model.Products{}
+	for rows.Next() {
+		product := model.Product{}
+		err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.Quantity,
+			&product.Category, &product.Description)
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+				"status": false,
+				"err":    err,
+			})
+		}
+		result = append(result, product)
+	}
+
+	// return products in JSON format
+	err = c.JSON(&fiber.Map{
+		"success": true,
+		"data":    result,
+		"message": "All products returned successfully",
+	})
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"status": false,
+			"err":    err,
+		})
+	}
+
+	return err
 }
 
 func GetProduct(c *fiber.Ctx) error {
--- model/product.go
+++ model/product.go
@@ -1,1 +1,12 @@
 package model
+
+type Product struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+	Quantity int `json:"quantity"`
+	Category string `json:"category"`
+	Description string `json:"description"`
+}
+
+type Products []Product
