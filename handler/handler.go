296330aa5bc0262b2d2cb435a7f8e0a02b19a157
Verrol Adams <verrol@lorrev.org>
2023-07-18 12:39:07 -0400

    simplified Product handlers to only rely on HTTP status codes instead

--- handler/handler.go
+++ handler/handler.go
@@ -14,10 +14,7 @@ func GetAllProducts(c *fiber.Ctx) error {
 	query := `SELECT * FROM products order by name`
 	rows, err := database.DB.Query(query)
 	if err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"status": false,
-			"err":    err,
-		})
+		return c.SendStatus(http.StatusInternalServerError)
 	}
 
 	defer rows.Close()
@@ -27,28 +24,15 @@ func GetAllProducts(c *fiber.Ctx) error {
 		err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.Quantity,
 			&product.Category, &product.Description)
 		if err != nil {
-			return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-				"status": false,
-				"err":    err,
-			})
+			return c.SendStatus(http.StatusInternalServerError)
 		}
 		result = append(result, product)
 	}
 
-	// return products in JSON format
-	err = c.JSON(&fiber.Map{
-		"success": true,
-		"data":    result,
-		"message": "All products returned successfully",
-	})
-	if err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"status": false,
-			"err":    err,
-		})
-	}
+	log.Info("query successfull", "query", query, "records", len(result))
 
-	return nil
+	// return products in JSON format
+	return c.JSON(result)
 }
 
 func GetProduct(c *fiber.Ctx) error {
@@ -58,10 +42,7 @@ func GetProduct(c *fiber.Ctx) error {
 	row, err := database.DB.Query(query, id)
 	if err != nil {
 		log.Info("query error", "id", id, "err", err)
-		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"success": false,
-			"message": err,
-		})
+		return c.SendStatus(http.StatusInternalServerError)
 	}
 
 	defer row.Close()
@@ -69,70 +50,36 @@ func GetProduct(c *fiber.Ctx) error {
 		err = row.Scan(&product.ID, &product.Name, &product.Price, &product.Quantity,
 			&product.Category, &product.Description)
 	} else {
-		log.Info("No rows were returned!", "id", id, "err", err)
-		return c.Status(http.StatusNotFound).JSON(&fiber.Map{
-			"success": false,
-			"message": err,
-		})
+		log.Info("No record found!", "id", id, "err", err)
+		return c.SendStatus(http.StatusNotFound)
 	}
 
 	if err != nil {
 		log.Error("Error while scanning product", "id", id, "err", err)
-		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"success": false,
-			"message": err,
-		})
+		return c.SendStatus(http.StatusInternalServerError)
 	}
 
 	log.Info("Product found", "id", id, "name", product.Name)
-	err = c.JSON(&fiber.Map{
-		"success": true,
-		"data":    product,
-	})
-	if err != nil {
-		c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"success": false,
-			"message": err,
-		})
-	}
-	return nil
+	return c.JSON(product)
 }
 
 func CreateProduct(c *fiber.Ctx) error {
 	p := new(model.Product)
 	if err := c.BodyParser(p); err != nil {
 		log.Error("Error while parsing product", "err", err)
-		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"success": false,
-			"message": err,
-		})
+		return c.SendStatus(http.StatusInternalServerError)
 	}
-	log.Info("Product parsed", "name", p.Name)
+
+	log.Info("Product parsed from request", "name", p.Name)
 	query := `INSERT INTO products (name, price, quantity, category, description) VALUES ($1, $2, $3, $4, $5) RETURNING id`
 	err := database.DB.QueryRow(query, p.Name, p.Price, p.Quantity, p.Category, p.Description).Scan(&p.ID)
 	if err != nil {
 		log.Error("Error while inserting product", "err", err)
-		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"success": false,
-			"message": err,
-		})
+		return c.SendStatus(http.StatusInternalServerError)
 	}
 
 	log.Info("Product inserted", "id", p.ID)
-	err = c.JSON(&fiber.Map{
-		"success": true,
-		"data":    p,
-		"message": "Product created successfully",
-	})
-	if err != nil {
-		log.Error("Error while sending product", "err", err)
-		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"success": false,
-			"message": err,
-		})
-	}
-
-	return nil
+	return c.JSON(p)
 }
 
 func DeleteProduct(c *fiber.Ctx) error {
@@ -141,24 +88,9 @@ func DeleteProduct(c *fiber.Ctx) error {
 	_, err := database.DB.Exec(query, id)
 	if err != nil {
 		log.Error("Error while deleting product", "err", err, "id", id)
-		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"success": false,
-			"message": err,
-		})
+		return c.SendStatus(http.StatusInternalServerError)
 	}
 
 	log.Info("Product deleted", "id", id)
-	err = c.JSON(&fiber.Map{
-		"success": true,
-		"message": "Product deleted successfully",
-	})
-	if err != nil {
-		log.Error("Error while sending product", "err", err)
-		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"success": false,
-			"message": err,
-		})
-	}
-	
-	return nil
+	return c.SendStatus(http.StatusOK)
 }
